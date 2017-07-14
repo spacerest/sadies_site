dca14efe15a23a03e29f512ce5edb3d0242478c0
spacerest <robconvo@gmail.com>
2017-07-14 12:58:56 -0500

    added projects model and template

--- website/views.py
+++ website/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.db import models
+from website.models import Project
 
 def index(request):
     return render(request, 'index.html')
 
 def findpage(request, nav):
+    if nav == 'projects':
+        project_list = Project.objects.all()
+        return render(request, 'projects.html', { 'projects': project_list, 'nav':nav, 'page_title': 'Home' }) 
     return render(request, '%s.html' % nav, {'page_title': 'Home', 'nav':nav})
