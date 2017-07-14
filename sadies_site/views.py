0b44a59539f8e846b7263b37acc6ecf1fc412d2c
spacerest <robconvo@gmail.com>
2017-07-14 12:58:56 -0500

    added projects model and template

---
+++ sadies_site/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+
--- website/models.py
+++ website/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length = 1000000, null = True, blank = True)
+    description = models.TextField(max_length = 1000000, null = True, blank = True)
+    link = models.URLField(max_length = 1000000, default = "https://sadieparker.net")
+    image_name = models.CharField(max_length = 2000, default = "default")
+
