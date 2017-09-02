4871c155ee023528b092dc1df1dd7290cfa7c88e
spacerest <robconvo@gmail.com>
2017-09-02 11:33:32 -0500

    style

--- website/static/website/assets/scripts/app.js
+++ website/static/website/assets/scripts/app.js
@@ -5,4 +5,20 @@ function scrollToId(x){
 	}, 1000);
 };
 
+function expandMail() {
+	$(".reply-emails").animate({height:220},300);
+}
 
+function hideMail() {
+	$(".reply-emails").animate({height:0},200);
+
+}
+
+$(window).scroll(function() {
+	var viewPortHeight = $(window).height();
+	if ($(document).scrollTop() > viewPortHeight / 2) {
+		$('.back-to-top').addClass('bot-show');
+	} else {
+		$('.back-to-top').removeClass('bot-show');
+	};
+});
