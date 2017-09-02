b6cadf626bb0ecc904ca5f0f9c24f52eb835389d
spacerest <robconvo@gmail.com>
2017-09-02 18:17:35 -0500

    style

---
+++ readme.md
@@ -0,0 +1,8 @@
+todo:
+
+- change lineheights for mobile
+- change email links for mobile
+- check for browser grid support 
+- add project descriptions to template
+- set debug to false!
+- add priorities to projects
--- website/models.py
+++ website/models.py
@@ -14,9 +14,10 @@ class Project(models.Model):
     project_type = models.ForeignKey(ProjectType, models.SET_NULL, null=True,blank=True)
     title = models.CharField(max_length = 1000000, null = True, blank = True)
     description = models.TextField(max_length = 1000000, null = True, blank = True)
-    link = models.URLField(max_length = 1000000, default = "https://sadieparker.net")
+    link = models.URLField(max_length = 1000000, blank = True, null = True)
     proj_image = models.ImageField(blank = True, null = True)
     last_updated = models.DateField(null = True, blank = True)
     tools_used = models.CharField(max_length = 100000, null = True, blank = True)
+    source_code = models.URLField(max_length = 100000, blank = True, null = True)
     def __str__(self):
         return self.title
