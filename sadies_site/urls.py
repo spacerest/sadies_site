fbd9959b68db12c16d9d8a4c735b7dcc379b4ccc
spacerest <robconvo@gmail.com>
2017-09-02 12:47:08 -0500

    static media setup & model update for projects

--- sadies_site/urls.py
+++ sadies_site/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 from website.views import * 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index),
     url(r'^(?P<nav>\w{0,20})/$', findpage)
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
