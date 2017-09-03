511a9859251ffff0b5f5e05572eb9245d3ce4b20
spacerest <robconvo@gmail.com>
2017-09-03 11:04:33 -0500

    updated media roots and some styling

--- website/models.py
+++ website/models.py
@@ -3,10 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.core.files.storage import FileSystemStorage
+
+fs = FileSystemStorage(location="/usr/share/nginx/html/media/")
+
 # Create your models here.
 
 class ProjectType(models.Model):
     title = models.CharField(max_length = 1000)
+    priority = models.IntegerField(null = True, blank = True)
     def __str__(self):
         return self.title
 
@@ -15,9 +20,10 @@ class Project(models.Model):
     title = models.CharField(max_length = 1000000, null = True, blank = True)
     description = models.TextField(max_length = 1000000, null = True, blank = True)
     link = models.URLField(max_length = 1000000, blank = True, null = True)
-    proj_image = models.ImageField(blank = True, null = True)
+    proj_image = models.ImageField(upload_to = fs, blank = True, null = True)
     last_updated = models.DateField(null = True, blank = True)
     tools_used = models.CharField(max_length = 100000, null = True, blank = True)
     source_code = models.URLField(max_length = 100000, blank = True, null = True)
+    priority = models.IntegerField(null = True, blank = True)
     def __str__(self):
         return self.title
